34cffbd56b3b8bc74094b6afd166256f7e9aba20
Wasif <wasif.huda@gmail.com>
2015-02-14 12:18:51 -0600

    extra credit

--- UITableViewController Challenge Solution/ChallengeUITableViewController.m
+++ UITableViewController Challenge Solution/ChallengeUITableViewController.m
@@ -34,24 +34,49 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    if(section== 0)
+    {
+        return 2;
+    }else if(section ==1)
+    {
+        return 1;
+    }else
+    {
+        return 3;
+    }
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     
     // Configure the cell...
     
+    
+    if(indexPath.section ==0)
+    {
+        cell.textLabel.text =[NSString stringWithFormat:@"I am in section %li", (long)indexPath.section];
+        cell.backgroundColor = [UIColor redColor];
+    }else if (indexPath.section ==1)
+    {
+        cell.textLabel.text =  @"Another section";
+        cell.backgroundColor = [UIColor blueColor];
+    }else
+    {
+        cell.textLabel.text = [NSString stringWithFormat:@"Cell %li", (long)indexPath.row];
+        cell.backgroundColor = [UIColor yellowColor];
+    }
+    
+    
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
